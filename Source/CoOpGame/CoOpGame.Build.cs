9e6683c2d3cc5909302e6fecaffada83897dcff6
eyupalemdar <eyupalemdar@outlook.com>
2020-09-21 14:19:00 +0300

    AI TrackerBot is added

--- Source/CoOpGame/CoOpGame.Build.cs
+++ Source/CoOpGame/CoOpGame.Build.cs
@@ -8,7 +8,14 @@ public class CoOpGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { 
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"UMG", 
+			"NavigationSystem" 
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
